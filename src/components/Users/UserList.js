2dbaeccf62741e7fb6615f939eadb7ccfc65ea2c
karnovmi <karnoumikhail@gmail.com>
2023-03-24 12:27:25 +0300

    made catchers of loaders and errors(useHook)-like it

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,36 +1,40 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, useCallback} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {addUser, fetchUsers} from '../../store'
 import {Skeleton} from "../Loader/Skeleton";
 import Button from "../UI/Button/Button";
 
-const UsersList = () => {
-    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-    const [loadingUsersError, setLoadingUsersError] = useState(null);
-    const [isCreatingUser, setIsCreatingUser] = useState(false);
-    const [creatingUserError, setCreatingUserError] = useState(null)
-
+const useThunk = (thunk)=>{
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
+
+    const runThunk = useCallback(()=>{
+        setIsLoading(true)
+        dispatch(thunk())
+            .unwrap()
+            .catch((err)=>setError(err))
+            .finally(()=>setIsLoading(false))
+    },[dispatch,thunk])
+
+    return [runThunk, isLoading, error]
+}
+
+const UsersList = () => {
+    const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers)
+    const [doAddUsers, isCreatingUser, creatingUserError] = useThunk(addUser)
 
     const { usersDataList} = useSelector((state) => {
         return state.users;
     })
 
     const handleAddUser = ()=>{
-        setIsCreatingUser(true)
-        dispatch(addUser())
-            .unwrap()
-            .catch(err => setCreatingUserError(err))
-            .finally(()=>setIsCreatingUser(false))
+        return doAddUsers()
     }
 
     useEffect(() => {
-        setIsLoadingUsers(true)
-        dispatch(fetchUsers())
-            .unwrap()
-            .catch((err)=>setLoadingUsersError(err))
-            .finally(()=>setIsLoadingUsers(false))
-    }, [dispatch])
+        doFetchUsers()
+    }, [doFetchUsers])
 
     if (isLoadingUsers) {
         return (<Skeleton times={6} classNameProp={'h-10 w-full'}/>)
